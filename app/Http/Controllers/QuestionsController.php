4811326df1ddea734af262ac396084d2dcb796a9
Gabor Toth <44711404+Toth-Gabor@users.noreply.github.com>
2020-04-15 09:16:29 +0200

    show all details of questions in basic style

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Question;
 
 class QuestionsController extends Controller
 {
     public function index()
     {
-        return view('questions.index');
+        $questionList = Question::all()->sortBy('submission_time', SORT_REGULAR,'desc');
+        //dd($questionList);
+        return view('questions.index', ['questionList' => $questionList]);
     }
 }
